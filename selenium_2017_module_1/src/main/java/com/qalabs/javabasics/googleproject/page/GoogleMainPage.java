8c7bbd4a9b8d5c5387f7796169e56c6a435bc09b
Roberto Maurer <r_m_s@hotmail.com>
2019-09-27 12:11:47 -0500

    #4 Agrega metodos hechos por Angelina

--- selenium_2017_module_1/src/main/java/com/qalabs/javabasics/googleproject/page/GoogleMainPage.java
+++ selenium_2017_module_1/src/main/java/com/qalabs/javabasics/googleproject/page/GoogleMainPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleMainPage extends BasePage {
   @FindBy(how = How.NAME, using = "q")
@@ -19,18 +20,37 @@ public class GoogleMainPage extends BasePage {
   private WebElement doodle;
 
   public GoogleMainPage(WebDriver driver) {
-    super(driver, "");
-    //this.doodle.submit();
+    super(driver, ""); // Falta definir la URL pero no la pongo porque no se si la vamos a traer de otro lado...
+
+    /* Esto lo comento por que no se si realmente lo vamos a necesitar -- Maurer
+    BasePage myPage = new BasePage();
+    myPage.open();
+    */
   }
-  public GoogleResultsPage searchInGoogle(String searchTxt) {
-    // Add Code
+  public GoogleResultsPage searchInGoogle(String searchTxt){
+    this.searchBox.sendKeys(searchTxt);
+    this.searchInGoogleButton.click;
+    return null;
   }
-  public GoogleResultsPage feelingLuckySearch(String searchTxt) {
-    // Add Code
+  public GoogleResultsPage feelingLuckySearch(String searchTxt){
+    this.searchBox.sendKeys(searchTxt);
+    this.feelingLuckyButton.click;
+    return null;
   }
 
   @Override
   public boolean isLoaded(){
-    // Add Code
+    // hay que revisar esto más a detalle
+    BasePage myBase = new BasePage();
+    try{
+      WebDriverWait wait= new  WebDriverWait(driver, 10);
+      wait.until(ExceptionConditions.VisibilityOf(double));
+      myBase.logger.info("Google main page loaded");
+      return true;
+    }catch(RunttimeException exception){
+      myBase.logger.error("Google main page was not load;"+ exception);
+      return false;
+    }
   }
+
 }
