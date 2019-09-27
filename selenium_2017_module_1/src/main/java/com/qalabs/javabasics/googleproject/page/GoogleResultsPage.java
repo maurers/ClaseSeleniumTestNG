0dc4c5e440845a6d31c3b124e2c649b40d3221f1
SigifredoPJ <sigifredopj@gmail.com>
2019-09-27 16:31:22 -0500

    JavierGoogleResultPages

--- selenium_2017_module_1/src/main/java/com/qalabs/javabasics/googleproject/page/GoogleResultsPage.java
+++ selenium_2017_module_1/src/main/java/com/qalabs/javabasics/googleproject/page/GoogleResultsPage.java
@@ -30,27 +30,44 @@ public class GoogleResultsPage extends BasePage {
   }
 
   public GoogleResultsPage search(String searchTxt) {
-    // Add Code
+       // Add Code
+    this.searchBox.sendKeys(searchTxt);
+    this.searchButton.click;
+    // return null;
   }
 
   public String currentSearch() {
     // Add Code
+  return this.searchBox.getText();
   }
 
   public List<ResultItem> getResults() {
     // Add Code
+  return.this.results;
   }
 
   public void nextResultPage() {
     // Add Code
+    this.nextButton.click();
   }
 
   public void prevResultPage() {
    // Add Code
+    this.prevButton.click();
   }
 
   @Override
   public boolean isLoaded() {
     // Add Code
+    BasePage myBase = new BasePage();
+    try{
+      WebDriverWait wait= new  WebDriverWait(driver, 10);
+      wait.until(ExceptionConditions.VisibilityOf(double));
+      myBase.logger.info("Google main page loaded");
+      return true;
+    }catch(RunttimeException exception){
+      myBase.logger.error("Google main page was not load;"+ exception);
+      return false;
+    }
   }
 }
